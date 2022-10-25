a5af25952554112968afc98fd7333e23251d8a3a
Caio Vinicius <caio.vin19@outlook.com>
2022-10-25 15:52:03 -0300

    first commit

---
+++ src/main/java/com/caio/springmongdb/WorkshopspringmongoApplication.java
@@ -0,0 +1,13 @@
+package com.caio.springmongdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopspringmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopspringmongoApplication.class, args);
+	}
+
+}
