1bb4b25aafa593882df57a9614307519b66d64ec
Caio Vinicius <caio.vin19@outlook.com>
2022-10-26 15:36:30 -0300

    insert

--- src/main/java/com/caio/springmongdb/resources/UserResource.java
+++ src/main/java/com/caio/springmongdb/resources/UserResource.java
@@ -1,14 +1,18 @@
 package com.caio.springmongdb.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.caio.springmongdb.domain.User;
 import com.caio.springmongdb.dto.UserDTO;
@@ -34,5 +38,12 @@ public class UserResource {
 		User obj=service.findById(id);		
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
-
+	
+	@PostMapping
+	public ResponseEntity<Void>insert(@RequestBody UserDTO objDto){
+		User obj=service.fromDTO(objDto);
+		URI uri=ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
 }
--- src/main/java/com/caio/springmongdb/services/UserService.java
+++ src/main/java/com/caio/springmongdb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.caio.springmongdb.domain.User;
+import com.caio.springmongdb.dto.UserDTO;
 import com.caio.springmongdb.repository.UserRepository;
 import com.caio.springmongdb.services.exception.ObjectNotFoundException;
 
@@ -24,6 +25,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
