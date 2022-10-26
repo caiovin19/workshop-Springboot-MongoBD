6f2b405775de21cacbe3bfe4fa3b7e630b12fa8c
Caio Vinicius <caio.vin19@outlook.com>
2022-10-26 14:07:36 -0300

    Instanciar bd

---
+++ src/main/java/com/caio/springmongdb/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.caio.springmongdb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.caio.springmongdb.domain.User;
+import com.caio.springmongdb.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll( Arrays.asList(maria,alex,bob));
+		
+	}
+
+}
