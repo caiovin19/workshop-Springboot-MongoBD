cc5d50b8081e30bb85476c3a7e82f25da36c5fc6
Caio Vinicius <caio.vin19@outlook.com>
2022-10-26 18:08:39 -0300

    obtendo post por id

---
+++ src/main/java/com/caio/springmongdb/services/PostService.java
@@ -0,0 +1,25 @@
+package com.caio.springmongdb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.caio.springmongdb.domain.Post;
+import com.caio.springmongdb.repository.PostRepository;
+import com.caio.springmongdb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+
+}
