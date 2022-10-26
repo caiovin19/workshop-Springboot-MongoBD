1e71a7ea3f61c3a7e672751d8aa838fd5b4d53cf
Caio Vinicius <caio.vin19@outlook.com>
2022-10-26 18:50:02 -0300

    busca por querymethods

--- src/main/java/com/caio/springmongdb/resources/PostResource.java
+++ src/main/java/com/caio/springmongdb/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.caio.springmongdb.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.caio.springmongdb.domain.Post;
+import com.caio.springmongdb.resources.util.URL;
 import com.caio.springmongdb.services.PostService;
 
 @RestController
@@ -22,5 +26,12 @@ public class PostResource {
 	public ResponseEntity<Post>findById(@PathVariable String id){
 		Post obj=service.findById(id);		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>>findByTitle(@RequestParam(value="text", defaultValue="") String text){
+		text=URL.decodeParam(text);
+		List<Post>list=service.findByTitle(text);		
+		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/com/caio/springmongdb/services/PostService.java
+++ src/main/java/com/caio/springmongdb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.caio.springmongdb.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,9 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
+	public List<Post>findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 	
 
 }
