3b37dbe8d142452ab58a5f064187307bf7c1482c
Caio Vinicius <caio.vin19@outlook.com>
2022-10-26 18:02:10 -0300

    endpont para retornar um comentario por id

--- src/main/java/com/caio/springmongdb/resources/UserResource.java
+++ src/main/java/com/caio/springmongdb/resources/UserResource.java
@@ -7,13 +7,13 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.caio.springmongdb.domain.Post;
 import com.caio.springmongdb.domain.User;
 import com.caio.springmongdb.dto.UserDTO;
 import com.caio.springmongdb.services.UserService;
@@ -59,6 +59,12 @@ public class UserResource {
 		obj.setId(id);
 		obj=service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>>findPosts(@PathVariable String id){
+		User obj=service.findById(id);		
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 }
