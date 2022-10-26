0d491cbecde7d6b45414c170d0d3947dff881d60
Caio Vinicius <caio.vin19@outlook.com>
2022-10-26 15:00:42 -0300

    usuario por id

--- src/main/java/com/caio/springmongdb/services/UserService.java
+++ src/main/java/com/caio/springmongdb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.caio.springmongdb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.caio.springmongdb.domain.User;
 import com.caio.springmongdb.repository.UserRepository;
+import com.caio.springmongdb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,6 +19,11 @@ public class UserService {
 	public List<User>findAll(){
 		return repo.findAll();
 		
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
---
+++ src/main/java/com/caio/springmongdb/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.caio.springmongdb.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException (String msg) {
+		super(msg);
+	}
+
+}
