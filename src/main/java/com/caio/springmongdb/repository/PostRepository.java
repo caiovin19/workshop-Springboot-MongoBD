e19455dd4f618e00e97d67fb8b7e5dde65b02c8b
Caio Vinicius <caio.vin19@outlook.com>
2022-10-26 18:57:28 -0300

    consulta simples com @Querry

--- src/main/java/com/caio/springmongdb/repository/PostRepository.java
+++ src/main/java/com/caio/springmongdb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.caio.springmongdb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.caio.springmongdb.domain.Post;
@@ -10,6 +11,10 @@ import com.caio.springmongdb.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
+	@Query("{'title':{$regex: ?0, $options : 'i'}}")
+	List<Post>searchTitle(String text);
 	
 	List<Post>findByTitleContainingIgnoreCase(String text);
+	
+	
 }
