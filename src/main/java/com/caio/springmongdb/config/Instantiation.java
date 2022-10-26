09a1cbe2ea19f0b7efa03b7491e88fdd1620790d
Caio Vinicius <caio.vin19@outlook.com>
2022-10-26 16:38:29 -0300

    post criado

--- src/main/java/com/caio/springmongdb/config/Instantiation.java
+++ src/main/java/com/caio/springmongdb/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.caio.springmongdb.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.caio.springmongdb.domain.Post;
 import com.caio.springmongdb.domain.User;
+import com.caio.springmongdb.repository.PostRepository;
 import com.caio.springmongdb.repository.UserRepository;
 
 @Configuration
@@ -14,18 +18,29 @@ public class Instantiation implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
 
 	
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf=new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		userRepository.saveAll( Arrays.asList(maria,alex,bob));
+		Post post1=new Post(null, sdf.parse("21/03/2018"), "Partil Viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2=new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
 		
+		
+		userRepository.saveAll( Arrays.asList(maria,alex,bob));
+		postRepository.saveAll( Arrays.asList(post1,post2));
 	}
 
 }
